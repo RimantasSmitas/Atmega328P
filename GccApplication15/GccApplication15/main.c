ee0ac0ebcca9a4334360eb9388ffefedcc165ced
rimantas <rimantas.smitas@gmail.com>
2018-05-25 00:08:14 +0200

    Update

---
+++ GccApplication15/GccApplication15/main.c
@@ -0,0 +1,18 @@
+/*
+ * GccApplication15.c
+ *
+ * Created: 5/16/2018 4:37:30 PM
+ * Author : riman
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
